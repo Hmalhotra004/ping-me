2d2a309e17ac6146a9befb8912b7fc3c102e4a58
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-05-13 17:59:06 +0530

    shit

--- app/(chat)/index.tsx
+++ app/(chat)/index.tsx
@@ -1,16 +1,37 @@
 import ItemTitleAndDescription from "@/components/ItemTitleAndDescription";
+import { appwriteConfig, db } from "@/utils/appwrite";
 import { colors } from "@/utils/colors";
 import { chatRooms } from "@/utils/test-data";
+import { ChatRoom } from "@/utils/types";
 import { useRouter } from "expo-router";
 import { CaretRight } from "phosphor-react-native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FlatList, Pressable, RefreshControl, StyleSheet } from "react-native";
+import { Query } from "react-native-appwrite";
 
 export default function Index() {
+  const [charRooms, setChatRooms] = useState<ChatRoom[]>([]);
   const [refresh, setRefresh] = useState(false);
   const router = useRouter();
 
+  async function fetchChatRooms() {
+    try {
+      const { documents, total } = await db.listDocuments(
+        appwriteConfig.db,
+        appwriteConfig.col.chatRooms,
+        [Query.limit(100)]
+      );
+      console.log(documents);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   async function handleRefresh() {}
+
+  useEffect(() => {
+    fetchChatRooms();
+  }, []);
 
   return (
     <FlatList
